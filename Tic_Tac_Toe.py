b8a26cf695aa36d5fddbe4ac2a1c618c3d812523
Manujkant <45746374+ManujKant@users.noreply.github.com>
2020-12-24 17:30:11 -0800

    created starting function to figure out who goes first

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,26 +1,66 @@
 from classes import Board
 
-def printBoard(position):
+
+user = ""
+AI = ""
+
+
+def starting():
+    starting = input("Do you want to start (Y/N)? ").upper()
+    inputUnderstood = False
+    while inputUnderstood == False:
+        if starting == "Y":
+            user = "X"
+            AI = "O"
+            inputUnderstood = True
+        elif starting == "N":
+            user = "O"
+            AI = "X"
+            inputUnderstood = True
+        else:
+            starting = input("Could not understand your response. Please type Y or N. Do you want to start? ").upper()
+        return user, AI
+
+
+
+def main(position):
     print("\n")
     print("|__% s__|__% s__|__% s__|" % (position["(1,3)"], position["(2,3)"], position["(3,3)"]))
     print("|__% s__|__% s__|__% s__|" % (position["(1,2)"], position["(2,2)"], position["(3,2)"]))
     print("|__% s__|__% s__|__% s__|" % (position["(1,1)"], position["(2,1)"], position["(3,1)"]))
     print("\n")
-    move = input("Which move would you like to make?))
+    move = input("Which move would you like to make? ")
+    currentPosition[move] = "X"
+
+
 
 # User is always X and AI is always O
-# # Coordinates are top-right point of each square
+# Coordinates are top-right point of each square
 # Each square has three states, Blank, AI, and User.
-position = {
-    "(1,1)": " ",
-    "(1,2)": "O",
-    "(1,3)": " ",
-    "(2,1)": "X",
-    "(2,2)": "O",
-    "(2,3)": " ",
-    "(3,1)": " ",
-    "(3,2)": " ",
-    "(3,3)": "X"
+currentPosition = {
+    "(1,1)": "",
+    "(1,2)": "",
+    "(1,3)": "",
+    "(2,1)": "",
+    "(2,2)": "",
+    "(2,3)": "",
+    "(3,1)": "",
+    "(3,2)": "",
+    "(3,3)": ""
 }
 
-printBoard(position)
+startingPosition = {
+    "(1,1)": "",
+    "(1,2)": "",
+    "(1,3)": "",
+    "(2,1)": "",
+    "(2,2)": "",
+    "(2,3)": "",
+    "(3,1)": "",
+    "(3,2)": "",
+    "(3,3)": ""
+}
+
+
+if __name__ == "__main__":
+    main(startingPosition)
--- classes.py
+++ classes.py
@@ -3,3 +3,4 @@ class Board:
     def __init__(self, position, myTurn):
         self.position = position
         self.myTurn = myTurn
+    
