aa6c6068db954ba01c6134f16531d85c91779f15
Manujkant <45746374+ManujKant@users.noreply.github.com>
2020-12-29 00:22:57 -0800

    have working program but engine isnt always winning

--- Minimax/main.py
+++ Minimax/main.py
@@ -1,38 +1,199 @@
-from engine import Board
+class Board:
 
+    def __init__(self, position, AI, user):
+        self.position = position
+        # AI is just X or O
+        self.AI = AI
+        self.user = user
 
-def starting():
-    starting = input("Do you want to start (Y/N)? ").upper()
-    inputUnderstood = False
-    while inputUnderstood == False:
-        if starting == "Y":
-            user = "X"
-            AI = "O"
-            inputUnderstood = True
-        elif starting == "N":
-            user = "O"
-            AI = "X"
-            inputUnderstood = True
-        else:
-            starting = input("Could not understand your response. Please type Y or N. Do you want to start? ").upper()
-        return user, AI
     
+    def emptyBoxes(self, position):
+
+        emptyBoxes = []
+        count = -1
+        for box in position:
+            count += 1
+            if box == " ":
+                emptyBoxes.append(count)
+
+        return emptyBoxes
+    
+    def findPossibilities(self, position, value):
+
+        emptyBoxes = self.emptyBoxes(position)
+
+        possibilities = []
+
+        for index in emptyBoxes:
+            position[index] = f"{value}"
+            possibilities.append(position[:])
+            position[index] = " "
+
+        return possibilities
+
+    def evaluatePosition(self, position):
+        # rows
+        for i in range(0, 7, 3):
+            if position[i] == position[i+1]:
+                if position [i] == position[i+2]:
+                    if position[i] == " ":
+                        pass
+                    elif position[i] == self.user:
+                        return -1
+                    elif position[i] == self.AI:
+                        return 1
+        # columns
+        for i in range(3):
+            if position[i] == position[i+3]:
+                if position[i] == position[i+6]:
+                    if position[i] == " ":
+                        pass
+                    elif position[i] == self.user:
+                        return -1
+                    elif position[i] == self.AI:
+                        return 1
+        # diagonals
+
+        if position[0] == position[4]:
+            if position[0] == position[8]:
+                if position[0] == " ":
+                    pass
+                elif position[0] == self.user:
+                    return -1
+                elif position[0] == self.AI:
+                    return 1
+        
+        if position[2] == position[4]:
+            if position[2] == position[6]:
+                if position[2] == " ":
+                    pass
+                elif position[2] == self.user:
+                    return -1
+                elif position[2] == self.AI:
+                    return 1
+        if self.emptyBoxes(position) == []:
+            return 0
+        return None
+        
+    def minimaxAlgorithm(self, position, maximizing):
+        if self.evaluatePosition(position) != None:
+            return self.evaluatePosition(position)
+            
+        if maximizing == True:
+            evaluations = []
+            for possibility in self.findPossibilities(position, self.AI):
+                evaluation = self.minimaxAlgorithm(possibility, False)
+                evaluations.append(evaluation)
+            return max(evaluations)
+
+        else:
+            evaluations = []
+            for possibility in self.findPossibilities(position, self.user):
+                evaluation = self.minimaxAlgorithm(possibility, True)
+                evaluations.append(evaluation)
+            return min(evaluations)
+            
+    def bestMove(self):
+        if self.evaluatePosition(self.position) == None:
+            bestMove = []
+            eval = -2
+            for position in self.findPossibilities(self.position, self.AI):
+                evaluation = self.minimaxAlgorithm(position, True)
+                if evaluation > eval:
+                    eval = evaluation
+                    bestMove = position
+        
+            return bestMove
+        else:
+            result = self.evaluatePosition(self.position)
+            return result
 
 
 
-def main(position):
+starting = input("Do you want to start (Y/N)? ").upper()
+inputUnderstood = False
+while inputUnderstood == False:
+    if starting == "Y":
+        user = "X"
+        AI = "O"
+        inputUnderstood = True
+    elif starting == "N":
+        user = "O"
+        AI = "X"
+        inputUnderstood = True
+    else:
+        starting = input("Could not understand your response. Please type Y or N. Do you want to start? ").upper()
+ 
+
+
+def X(position):
+
+
+    move = Board(position, AI, user)
+    if move.evaluatePosition(position) != None:
+        print("\n")
+        print(" % s  |  % s  |  % s  " % (position[6], position[7], position[8]))
+        print("______________")
+        print(" % s  |  % s  |  % s  " % (position[3], position[4], position[5]))
+        print("_______________")
+        print(" % s  |  % s  |  % s  " % (position[0], position[1], position[2]))
+        print("_______________")
+        
+        if move.evaluatePosition(position) == 1:
+            print("I win")
+        if move.evaluatePosition(position) == 0:
+            print("Draw")
+        if move.evaluatePosition(position) == -1:
+            print("Good Job")
+  
+
+    if move.evaluatePosition(position) == None:
+        newPosition = move.bestMove()
+        eval = move.evaluatePosition(newPosition)
+        print("\n")
+        print(" % s  |  % s  |  % s  " % (newPosition[6], newPosition[7], newPosition[8]))
+        print("______________")
+        print(" % s  |  % s  |  % s  " % (newPosition[3], newPosition[4], newPosition[5]))
+        print("_______________")
+        print(" % s  |  % s  |  % s  " % (newPosition[0], newPosition[1], newPosition[2]))
+        print("_______________")
+        if eval == 1:
+            print("I win")
+        elif eval == 0:
+            print("Draw")
+        elif eval== -1:
+            print("Good Job")
+        if eval == None:
+            inputUnderstood = False
+            while inputUnderstood == False:
+                move = input("Which move would you like to make? ")
+                if newPosition[int(move)-1] == " ":
+                    newPosition[int(move)-1] = user
+                    inputUnderstood = True
+                else:
+                    print("That square is taken, pick another square. (1-9)")
+            X(newPosition)
+
+def O():
+    
+    startingPosition = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
     print("\n")
-    print(" % s  |  % s  |  % s  " % (position[6], position[7], position[8]))
-    print("________________")
-    print(" % s  |  % s  |  % s  " % (position[3], position[4], position[7]))
-    print("________________")
-    print(" % s  |  % s  |  % s  " % (position[0], position[1], position[2]))
-    print("________________")
+    print("    |     |     ")
+    print("______________")
+    print("    |     |     ")
+    print("_______________")
+    print("    |     |     ")
+    print("_______________")
+
     move = input("Which move would you like to make? ")
+    startingPosition[int(move) - 1] = user
 
 
+    X(startingPosition)
 
 
-
-if __name__ == "__main__":
-    starting()
+if AI == "X":
+    X()
+else:
+    O()
